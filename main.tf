82018d6c2c37526f84abaaf2c55edf25eafe2070
Terenzano <naikatepa@gmail.com>
2024-07-22 17:20:12 +0300

    Added provider details in main file

--- main.tf
+++ main.tf
@@ -8,6 +8,12 @@ terraform {
   }
 }
 
+provider "aci" {
+  username = "admin"
+  password = "!v3G@!4@Y"
+  url      = "https://sandboxapicdc.cisco.com"
+}
+
 module "aci" {
   source  = "netascode/nac-aci/aci"
   version = "0.9.0"
